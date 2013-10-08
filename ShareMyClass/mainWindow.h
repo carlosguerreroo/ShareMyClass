72675407026faa311793da95e9d9ff1185f950b0
Carlos <araogaraog@gmail.com>
2013-10-08 08:35:17 -0500

    Adds other viewController called mainWindow

---
+++ ShareMyClass/mainWindow.h
@@ -0,0 +1,13 @@
+//
+//  mainWindow.h
+//  ShareMyClass
+//
+//  Created by carlos omana on 08/10/13.
+//  Copyright (c) 2013 ITESM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface mainWindow : UIViewController
+
+@end
---
+++ ShareMyClass/mainWindow.m
@@ -0,0 +1,38 @@
+//
+//  mainWindow.m
+//  ShareMyClass
+//
+//  Created by carlos omana on 08/10/13.
+//  Copyright (c) 2013 ITESM. All rights reserved.
+//
+
+#import "mainWindow.h"
+
+@interface mainWindow ()
+
+@end
+
+@implementation mainWindow
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
