7160bc56d280c6b74524ef7ac7aa939958a8bdef
Vicente Balderas <vbm1191@gmail.com>
2013-11-24 16:31:29 -0600

    Upload files incomplete

--- ShareMyClass/NewFileViewController.m
+++ ShareMyClass/NewFileViewController.m
@@ -85,7 +85,29 @@
     }
     else
     {
+       /* NSData *imageData = UIImagePNGRepresentation(self.showImageOutlet.image);
+        NSString *urlString = [NSString stringWithFormat:@"%@test.php", delegate.dataBean.hosterURL];
         
+        NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+        [request setURL:[NSURL URLWithString:urlString]];
+        [request setHTTPMethod:@"POST"];
+        
+        NSString *boundary = [NSString stringWithString:@"---------------------------14737809831466499882746641449"];
+        NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@",boundary];
+        [request addValue:contentType forHTTPHeaderField: @"Content-Type"];
+        
+        NSMutableData *body = [NSMutableData data];
+        [body appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n",boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"uploadedfile\"; filename=\"test.png\"\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[[NSString stringWithString:@"Content-Type: application/octet-stream\r\n\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[NSData dataWithData:imageData]];
+        [body appendData:[[NSString stringWithFormat:@"\r\n--%@--\r\n",boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+        [request setHTTPBody:body];
+        
+        NSData *returnData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+        NSString *returnString = [[NSString alloc] initWithData:returnData encoding:NSUTF8StringEncoding];
+        
+        NSLog([NSString stringWithFormat:@"Image Return String: %@", returnString]);*/
     }
 }
 
